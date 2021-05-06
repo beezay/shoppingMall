f8b619fa8ebe04d0e0a6a65082bea948dd448eeb
beezay bohora <bijay.okur@gmail.com>
2021-05-06 13:01:19 +0545

    Toast for Delete created and Implemented to all

--- src/components/HomePage/Malls.js
+++ src/components/HomePage/Malls.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { deleteMallStorage } from "../../utils/Delete";
 import Card from "../common/Card";
+import DeleteAlert from "../common/DeleteAlert";
 
 const Malls = ({ filterMalls }) => {
+  const [deleteToast, setDeleteToast] = useState(false);
   const history = useHistory();
 
   const handleSingleMallClick = (mallId) => {
@@ -16,29 +18,36 @@ const Malls = ({ filterMalls }) => {
     let confirm = window.confirm("Are you sure to Delete Mall??");
     if (confirm) {
       await deleteMallStorage(filterMalls, mallId);
+      setDeleteToast(true);
+      setTimeout(() => {
+        setDeleteToast(false);
+      }, 1500);
     }
   };
 
   return (
-    <div className="malls-wrapper">
-      <div className="mall-heading">
-        <h2>MALLS</h2>
+    <>
+      {deleteToast && <DeleteAlert />}
+      <div className="malls-wrapper">
+        <div className="mall-heading">
+          <h2>MALLS</h2>
+        </div>
+        <div className="image-wrapper">
+          {filterMalls.map((mall) => (
+            <Card
+              className="image-container"
+              func={handleSingleMallClick}
+              name={mall.mallName}
+              address={mall.mallAddress}
+              imgUrl={mall.mallImage.imageUrl}
+              key={mall.id}
+              id={mall.id}
+              onMallDelete={handleSingleMallDelete}
+            />
+          ))}
+        </div>
       </div>
-      <div className="image-wrapper">
-        {filterMalls.map((mall) => (
-          <Card
-            className="image-container"
-            func={handleSingleMallClick}
-            name={mall.mallName}
-            address={mall.mallAddress}
-            imgUrl={mall.mallImage.imageUrl}
-            key={mall.id}
-            id={mall.id}
-            onMallDelete={handleSingleMallDelete}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
